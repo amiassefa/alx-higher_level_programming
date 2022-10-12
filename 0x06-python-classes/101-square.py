37ee854307d0ba9a563b98fb7a80d8da91799893
amiassefa <amiassefa@gmail.com>
2022-10-12 11:01:12 +0300

    Python clases

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,82 +1,76 @@
+
+
 #!/usr/bin/python3
-"""Square module."""
+"""
+    This module defines linked lists
+    return {}
+"""
 
 
-class Square:
-    """Defines a square."""
-
-    def __str__(self):
-        """Returns string representation."""
-        return self.my_sprint()[:-1]
+class Square():
+    """square class"""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Constructor.
-
-        Args:
-            size: Length of a side of the square.
-            position: Position of the square.
-        """
+        """Class Initialization"""
         self.size = size
         self.position = position
 
+    def area(self):
+        """Returning the area of the square"""
+        return self.__size ** 2
+
     @property
     def size(self):
-        """Property for the length of a side of this square.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
-        """
+        """Retrieving the size of Square"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Setting the size of Square"""
         if not isinstance(value, int):
-            raise TypeError('size must be an integer')
+            raise TypeError("size must be an integer")
         if value < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError("size must be >= 0")
         self.__size = value
+
+    def my_print(self):
+        """Printing the square with the char #"""
+        if self.__size == 0:
+            print()
+            return
+        for row in range(self.__position[1]):
+            print("")
+        for i in range(self.__size):
+            for space in range(self.__position[0]):
+                print(" ", end="")
+            for j in range(self.__size):
+                print("#", end="")
+            print()
 
     @property
     def position(self):
-        """Property for the position of this square.
-
-        Raises:
-            TypeError: If value is not tuple of 2 positive integers.
-        """
+        """Retrieving the position of Square"""
         return self.__position
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2 or \
-         len([x for x in value if isinstance(x, int) and x >= 0]) != 2:
-            raise TypeError('position must be a tuple of 2 positive integers')
+        """Setting the position of Square"""
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if any(isinstance(value, int) and num < 0 for num in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
-    def area(self):
-        """Area of this square.
-
-        Returns:
-            The size squared.
-        """
-        return self.__size ** 2
-
-    def my_sprint(self):
-        """Returns string representation of this square."""
-        ret = ""
-        if not self.size:
-            return "\n"
-
-        for i in range(self.position[1]):
-                ret += "\n"
-        for i in range(self.size):
-            for j in range(self.position[0]):
-                ret += " "
-            for j in range(self.size):
-                ret += "#"
-            ret += "\n"
-        return ret
-
-    def my_print(self):
-        """Prints this square."""
-        print(self.my_sprint(), end="")
+    def __repr__(self):
+        string = ""
+        if self.__size == 0:
+            return ""
+        for row in range(self.__position[1]):
+            string += "\n"
+        for x in range(self.__size):
+            for space in range(self.__position[0]):
+                string += " "
+            for i in range(self.__size):
+                string += "#"
+            string += "\n"
+        return string[:-1]
