b668a65f5ad903bec30fad192b5424d5e349051c
amiassefa <amiassefa@gmail.com>
2022-10-25 20:01:03 +0300

    Python Input Output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+"""
+Append module
+"""
+
+
 def append_write(filename="", text=""):
-    with open(filename, mode='a', encoding='utf-8') as f:
-        return f.write(text)
+    """Append to file with text
+    args:
+        filename: add to this object
+        text: string to append
+    return:
+        number of characters added
+    """
+
+    with open(filename, mode="a", encoding="utf-8") as f:
+        if f.write(text):
+            return len(text)
