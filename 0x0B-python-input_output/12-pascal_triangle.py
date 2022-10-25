85b9edd91e829e0c9a16fde21ae49be2372bb7f7
amiassefa <amiassefa@gmail.com>
2022-10-25 21:16:21 +0300

    Python Input Output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
+
 """
-Pascal's Triangle Module
+pascal_triangle Module
 """
 
 
 def pascal_triangle(n):
-    """calculate pascal
-    args:
-        n: value
-    return
-        list
-    """
-    my_list = []
+    """Returns a list of lists of integers of n"""
+    res = []
     if n <= 0:
-        return my_list
-    for i in range(n):
-        num = 11**i
-        li = [int(n) for n in str(num)]
-        my_list.append(li)
-    return my_list
+        return res
+    for x in range(n):
+        row = [1]
+        if x > 0:
+            for j in range(x):
+                row.append(sum(res[-1][j:j + 2]))
+        res.append(row)
+    return res
