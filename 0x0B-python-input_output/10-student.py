6e1c3dd890b71a5e1f23062e3e8865f41c8d0f64
amiassefa <amiassefa@gmail.com>
2022-10-25 21:22:39 +0300

    Python Input Output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,30 +1,28 @@
+
+
 #!/usr/bin/python3
+
 """
-Class Module
+Student class Module
 """
 
 
 class Student:
-    """
-    Student class
-    """
+    """Student class"""
 
     def __init__(self, first_name, last_name, age):
-        """initialize method
-        """
+        """Initialization of the class"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves a dictionary representation of student instance
-        args:
-            attrs: attributes
-        return:
-            dictionary
-        """
-        if not attrs:
+        """Retrieving the dictionary representation of the Student instance."""
+        if attrs is not None and all(isinstance(x, str) for x in attrs):
+            d = {}
+            for k, v in self.__dict__.items():
+                if k in attrs:
+                    d[k] = v
+            return d
+        else:
             return self.__dict__
-
-        return ({key: value for key, value in self.__dict__.items()
-                 if key in attrs})
