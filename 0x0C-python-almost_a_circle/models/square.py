88963aeb85d96d72891c9a2e3f09e83b05c553e7
amiassefa <amiassefa@gmail.com>
2022-11-04 19:28:16 +0300

    Python almost a circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,53 +1,96 @@
+
+
 #!/usr/bin/python3
-"""square
+"""Module square
+This class Square that inherits from Rectangle
 """
+
+from models.base import Base
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Inherits from Rectangle
+    """This is a class describing a square
+    
+    Public instance methods:
+        - area()
+        - display()
+        - to_dictionary()
+        - update()
+        All inherit from Rectangle.
     """
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(width=size, height=size, x=x, y=y, id=id)
+        """Initializes a Square instance.
+        Args:
+            - __size: size
+            - __x: position
+            - __y: position
+            - id: id
+        """
+
+        self.size = size
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns a string representation of a Square instance."""
+
+        s = "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.__width)
+        return s
 
     @property
     def size(self):
-        return self.width
+        """Retrieves the size attribute."""
+
+        return self.__width
 
     @size.setter
     def size(self, value):
-        """size needs to be an int
-        """
+        """Sets the size attribute."""
 
-        self.width = value
-        self.height = value
-
-    def __str__(self):
-        """Returns formatted information display
-        """
-
-        return "[{}] ({}) {}/{} - {}".format(self.__class__.__name__,
-                                             self.id, self.x, self.y,
-                                             self.width)
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
 
     def update(self, *args, **kwargs):
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                setattr(self, k, v)
-        elif len(args) != 0:
-            try:
-                self.id = args[0]
-                self.size = args[1]
-                self.x = args[2]
-                self.y = args[3]
-            except IndexError:
-                pass
-        else:
-            print()
-
-    def to_dictionary(self):
-        """Returns a dict representation
+        """Updates attributes of an instance.
+        Args:
+            - id attribute
+            - size attribute
+            - x attribute
+            - y attribute
         """
 
-        return {'id': self.id, 'x': self.x, 'size': self.width, 'y': self.y}
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                if type(args[0]) != int and args[0] is not None:
+                    raise TypeError("id must be an integer")
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if type(value) != int and value is not None:
+                        raise TypeError("id must be an integer")
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Square."""
+
+        my_dict = {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+        return my_dict
